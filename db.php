1b98710dc55e0b918cbed86f8f1f5cbf98538856
CezarBernandino12 <bernandinocezar@gmail.com>
2026-02-11 08:32:55 +0800

    cezar connect

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'psi_database';
+$user = 'root';
+$pass = '';
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    exit('Database connection failed.');
+}
