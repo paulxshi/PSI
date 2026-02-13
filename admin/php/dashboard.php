9838e728eb7ab4d4af49363c827e6a02766543a8
CezarBernandino12 <bernandinocezar@gmail.com>
2026-02-13 14:45:46 +0800

    dashboard

---
+++ admin/php/dashboard.php
@@ -0,0 +1,37 @@
+<?php
+require "../../config/db.php";
+
+header('Content-Type: application/json');
+
+$sql = "SELECT
+            v.region,
+            v.venue_id,
+            v.venue_name,
+            s.schedule_datetime,
+            s.num_registered,
+            s.num_of_examinees
+        FROM venue v
+        LEFT JOIN schedules s ON s.venue_id = v.venue_id
+        ORDER BY v.region, v.venue_name, s.schedule_datetime";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$data = [];
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+    $region = $row['region'];
+    $venue  = $row['venue_name'];
+
+    if (!isset($data[$region])) $data[$region] = [];
+    if (!isset($data[$region][$venue])) $data[$region][$venue] = [];
+
+    $data[$region][$venue][] = [
+        "date" => $row['schedule_datetime'],
+        "registered" => $row['num_registered'],
+        "limit" => $row['num_of_examinees']
+    ];
+}
+
+echo json_encode($data);
