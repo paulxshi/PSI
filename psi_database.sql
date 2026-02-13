0f528df70ce72c884ab7638b8c872a19b3634c24
CezarBernandino12 <bernandinocezar@gmail.com>
2026-02-13 13:16:20 +0800

    db update

--- psi_database.sql
+++ psi_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2026 at 06:38 AM
+-- Generation Time: Feb 13, 2026 at 06:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `psi_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `examinees`
+--
+
+CREATE TABLE `examinees` (
+  `test_id` int(50) NOT NULL,
+  `user_id` int(50) NOT NULL,
+  `date_of_test` datetime NOT NULL,
+  `date_of_registration` date NOT NULL DEFAULT current_timestamp(),
+  `venue` varchar(250) NOT NULL,
+  `test_permit` varchar(50) NOT NULL,
+  `purpose` varchar(255) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `schedule_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `examinees`
+--
+
+INSERT INTO `examinees` (`test_id`, `user_id`, `date_of_test`, `date_of_registration`, `venue`, `test_permit`, `purpose`, `status`, `schedule_id`) VALUES
+(1, 1, '2026-02-15 12:00:00', '2026-02-11', 'Labo, Camarines Norte, Luzon', '90-8008-09', '', '0', NULL),
+(2, 3, '2026-02-11 02:23:02', '2026-02-11', 'Muntinlupa', '676-0808', '', 'Pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -53,55 +79,27 @@ CREATE TABLE `schedules` (
   `schedule_id` int(11) NOT NULL,
   `venue_id` int(11) NOT NULL,
   `schedule_datetime` datetime NOT NULL,
-  `num_of_examinees` int(11) NOT NULL
+  `num_of_examinees` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'Incoming'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `schedules`
 --
 
-INSERT INTO `schedules` (`schedule_id`, `venue_id`, `schedule_datetime`, `num_of_examinees`) VALUES
-(1, 1, '2026-02-17 00:00:00', 200),
-(2, 2, '2026-02-19 00:00:00', 100),
-(3, 3, '2026-02-23 00:00:00', 300),
-(4, 3, '2026-02-23 00:00:00', 300),
-(5, 4, '0000-00-00 00:00:00', 0),
-(6, 4, '0000-00-00 00:00:00', 0),
-(7, 2, '2026-02-16 00:00:00', 1000),
-(8, 2, '2026-10-23 00:00:00', 200),
-(9, 5, '2026-02-26 00:00:00', 250),
-(10, 1, '2026-03-04 00:00:00', 200),
-(11, 1, '2026-02-23 00:00:00', 300),
-(12, 1, '0000-00-00 00:00:00', 200),
-(13, 1, '0000-00-00 00:00:00', 500),
-(14, 1, '0000-00-00 00:00:00', 500),
-(15, 1, '2026-03-12 00:00:00', 400),
-(16, 1, '2026-04-04 00:00:00', 250);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test`
---
-
-CREATE TABLE `test` (
-  `test_id` int(50) NOT NULL,
-  `user_id` int(50) NOT NULL,
-  `date_of_test` datetime NOT NULL,
-  `date_of_registration` date NOT NULL DEFAULT current_timestamp(),
-  `venue` varchar(250) NOT NULL,
-  `test_permit` varchar(50) NOT NULL,
-  `purpose` varchar(255) NOT NULL,
-  `status` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `test`
---
-
-INSERT INTO `test` (`test_id`, `user_id`, `date_of_test`, `date_of_registration`, `venue`, `test_permit`, `purpose`, `status`) VALUES
-(1, 1, '2026-02-15 12:00:00', '2026-02-11', 'Labo, Camarines Norte, Luzon', '90-8008-09', '', '0'),
-(2, 3, '2026-02-11 02:23:02', '2026-02-11', 'Muntinlupa', '676-0808', '', 'Pending');
+INSERT INTO `schedules` (`schedule_id`, `venue_id`, `schedule_datetime`, `num_of_examinees`, `status`) VALUES
+(1, 1, '2026-02-17 00:00:00', 200, 'Incoming'),
+(2, 2, '2026-02-19 00:00:00', 100, 'Incoming'),
+(3, 3, '2026-02-23 00:00:00', 300, 'Incoming'),
+(4, 3, '2026-02-23 00:00:00', 300, 'Incoming'),
+(7, 2, '2026-02-16 00:00:00', 1000, 'Incoming'),
+(8, 2, '2026-10-23 00:00:00', 200, 'Incoming'),
+(9, 5, '2026-02-26 00:00:00', 250, 'Incoming'),
+(10, 1, '2026-03-04 00:00:00', 200, 'Incoming'),
+(11, 1, '2026-02-23 00:00:00', 300, 'Incoming'),
+(15, 1, '2026-03-12 00:00:00', 400, 'Incoming'),
+(16, 1, '2026-04-04 00:00:00', 250, 'Incoming'),
+(17, 1, '2026-03-04 00:00:00', 3000, 'Incoming');
 
 -- --------------------------------------------------------
 
@@ -111,34 +109,40 @@ INSERT INTO `test` (`test_id`, `user_id`, `date_of_test`, `date_of_registration`
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
+  `test_permit` varchar(32) DEFAULT NULL,
+  `pmma_student_id` varchar(32) DEFAULT NULL,
   `last_name` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `middle_name` varchar(1) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `contact_number` int(11) NOT NULL,
   `date_of_birth` date NOT NULL,
   `age` int(3) NOT NULL,
   `gender` varchar(6) NOT NULL,
+  `school` varchar(255) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `exam_venue` varchar(255) DEFAULT NULL,
+  `exam_date` date DEFAULT NULL,
+  `email_verified` tinyint(1) DEFAULT 0,
   `address` varchar(50) NOT NULL,
   `nationality` varchar(50) NOT NULL,
-  `contact_number` int(11) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `pmma_student_id` varchar(32) DEFAULT NULL,
-  `test_permit` varchar(32) DEFAULT NULL
+  `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `last_name`, `first_name`, `middle_name`, `email`, `date_of_birth`, `age`, `gender`, `address`, `nationality`, `contact_number`, `password`, `pmma_student_id`, `test_permit`) VALUES
-(1, 'Bernandino', 'Cezar', 'I', 'bernandinocezar@gmail.com', '2003-02-12', 22, '', 'P-3 Lugui LCN', '', 963288424, 'cezar1212', NULL, NULL),
-(2, 'Almadrones', 'Lee Ivan', 'O', 'leeivanalmadrones6@gmail.com', '2004-09-10', 21, '', 'Purok 3 Lugui Labo Camarines Norte', '', 2147483647, '$2y$10$S.dQ1huZ/X25vFuRwsC8qOaGN1ISJ//t/lVRNxH8Zl5E0OuEGWdi6', NULL, NULL),
-(3, 'Bernandino', 'Cezar', 'I', 'cezarbernandino12@gmail.com', '2026-02-03', 18, 'Male', 'Labo CN', 'FIlipino', 90908777, '$2y$10$aVHzKHAZFsXnhg/Qbu6FfO1p9zLwoyJ1Y7NlXbSSK9xnXK7/Dftj2', '22-1512', '09-0909-09'),
-(6, 'Bernandino', 'Cezar', 's', 'bernandinoceza@gmail.com', '2026-02-11', 0, 'male', '', '', 963234542, '$2y$10$hJJf7rVeavqbaEENsKOMEeZNQdE/buYQhXT5Rn6EUePaZUBy9hKOe', NULL, NULL),
-(15, 'Bernandino', 'Ceza', 's', 'bernandinocear@gmail.com', '2026-02-11', 0, '', '', '', 963234542, '$2y$10$3/VyMuYldB8.vWPq1CCjd.1wL57Tj2GLUAPfth1WU9nIHJGT9SUXu', NULL, NULL),
-(18, 'Bernando', 'Cezar', 'I', 'bernandocezar@gmail.com', '2026-02-11', 0, '', '', '', 963234542, '$2y$10$VP4STuI94nNvGvhUNuOFDu.I//pUPZDktMSVIgBa99OpaPsBlpU2.', NULL, NULL),
-(20, 'onidnanreb', 'Cezarrrr', 's', 'bernandinicezar@gmail.com', '2026-02-11', 0, 'male', '', '', 963234542, '$2y$10$EMlfA8JAk.hKfo3PSWxwqeEmNJ8PHB0pKPSWqv7tE5Ik.g6P/hVNq', NULL, NULL),
-(21, 'onidnanreb', 'Cezarrrr', 's', 'nandinocezar@gmail.com', '2026-02-12', 0, 'male', '', '', 963234542, '$2y$10$piPzS1RXw58S7Yep5ObS9.yPRveFNJ9sTKESPl.4eh/kuzyrQztt2', NULL, NULL);
+INSERT INTO `users` (`user_id`, `test_permit`, `pmma_student_id`, `last_name`, `first_name`, `middle_name`, `email`, `contact_number`, `date_of_birth`, `age`, `gender`, `school`, `region`, `exam_venue`, `exam_date`, `email_verified`, `address`, `nationality`, `password`, `status`) VALUES
+(1, NULL, NULL, 'Bernandino', 'Cezar', 'I', 'ernandinocezar@gmail.com', 963288424, '2003-02-12', 22, '', NULL, NULL, NULL, NULL, 0, 'P-3 Lugui LCN', '', 'cezar1212', ''),
+(2, NULL, NULL, 'Almadrones', 'Lee Ivan', 'O', 'leeivanalmadrones6@gmail.com', 2147483647, '2004-09-10', 21, '', NULL, NULL, NULL, NULL, 0, 'Purok 3 Lugui Labo Camarines Norte', '', '$2y$10$S.dQ1huZ/X25vFuRwsC8qOaGN1ISJ//t/lVRNxH8Zl5E0OuEGWdi6', ''),
+(3, '09-0909-09', '22-1512', 'Bernandino', 'Cezar', 'I', 'cezarbernandino12@gmail.com', 90908777, '2026-02-03', 18, 'Male', NULL, NULL, NULL, NULL, 0, 'Labo CN', 'FIlipino', '$2y$10$aVHzKHAZFsXnhg/Qbu6FfO1p9zLwoyJ1Y7NlXbSSK9xnXK7/Dftj2', ''),
+(6, NULL, NULL, 'Bernandino', 'Cezar', 's', 'bernandinoceza@gmail.com', 963234542, '2026-02-11', 0, 'male', NULL, NULL, NULL, NULL, 0, '', '', '$2y$10$hJJf7rVeavqbaEENsKOMEeZNQdE/buYQhXT5Rn6EUePaZUBy9hKOe', ''),
+(15, NULL, NULL, 'Bernandino', 'Ceza', 's', 'bernandinocear@gmail.com', 963234542, '2026-02-11', 0, '', NULL, NULL, NULL, NULL, 0, '', '', '$2y$10$3/VyMuYldB8.vWPq1CCjd.1wL57Tj2GLUAPfth1WU9nIHJGT9SUXu', ''),
+(18, NULL, NULL, 'Bernando', 'Cezar', 'I', 'bernandocezar@gmail.com', 963234542, '2026-02-11', 0, '', NULL, NULL, NULL, NULL, 0, '', '', '$2y$10$VP4STuI94nNvGvhUNuOFDu.I//pUPZDktMSVIgBa99OpaPsBlpU2.', ''),
+(20, NULL, NULL, 'onidnanreb', 'Cezarrrr', 's', 'bernandinicezar@gmail.com', 963234542, '2026-02-11', 0, 'male', NULL, NULL, NULL, NULL, 0, '', '', '$2y$10$EMlfA8JAk.hKfo3PSWxwqeEmNJ8PHB0pKPSWqv7tE5Ik.g6P/hVNq', ''),
+(21, NULL, NULL, 'onidnanreb', 'Cezarrrr', 's', 'nandinocezar@gmail.com', 963234542, '2026-02-12', 0, 'male', NULL, NULL, NULL, NULL, 0, '', '', '$2y$10$piPzS1RXw58S7Yep5ObS9.yPRveFNJ9sTKESPl.4eh/kuzyrQztt2', '');
 
 -- --------------------------------------------------------
 
@@ -160,12 +164,19 @@ INSERT INTO `venue` (`venue_id`, `venue_name`, `region`) VALUES
 (1, 'Manila', 'Luzon'),
 (2, 'Cebu', 'Visayas'),
 (3, 'Bicol', 'Luzon'),
-(4, '', ''),
-(5, '300', 'Luzon');
+(5, 'Davao', 'Mindanao');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `examinees`
+--
+ALTER TABLE `examinees`
+  ADD PRIMARY KEY (`test_id`),
+  ADD KEY `fk_test_user` (`user_id`),
+  ADD KEY `fk_test_schedule` (`schedule_id`);
 
 --
 -- Indexes for table `payments`
@@ -179,13 +190,6 @@ ALTER TABLE `payments`
 ALTER TABLE `schedules`
   ADD PRIMARY KEY (`schedule_id`),
   ADD KEY `fk_schedules_venue` (`venue_id`);
-
---
--- Indexes for table `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`test_id`),
-  ADD KEY `fk_test_user` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -209,6 +213,12 @@ ALTER TABLE `venue`
 --
 
 --
+-- AUTO_INCREMENT for table `examinees`
+--
+ALTER TABLE `examinees`
+  MODIFY `test_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
@@ -218,19 +228,13 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `test`
---
-ALTER TABLE `test`
-  MODIFY `test_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `venue`
@@ -243,16 +247,17 @@ ALTER TABLE `venue`
 --
 
 --
+-- Constraints for table `examinees`
+--
+ALTER TABLE `examinees`
+  ADD CONSTRAINT `fk_test_schedule` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`schedule_id`),
+  ADD CONSTRAINT `fk_test_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `schedules`
 --
 ALTER TABLE `schedules`
   ADD CONSTRAINT `fk_schedules_venue` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`venue_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `test`
---
-ALTER TABLE `test`
-  ADD CONSTRAINT `fk_test_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
