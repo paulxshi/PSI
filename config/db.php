bfb19e178663a61dc100d73182e73503f369e3e4
leeivanalmadrones <leeivanalmadrones6@gmail.com>
2026-03-01 11:01:44 +0800

    update

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = '127.0.0.1';
-$db   = 'u679207742_pmma';
-$user = 'u679207742_User_pmma';
-$pass = 'gZ3*FuQ@2DZ#';
+$db   = 'pmma_database';
+$user = 'root';
+$pass = '';
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 try {
